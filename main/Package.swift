46b9796ff5db37e928ef53b72586952e2e1bbd11
anders0nmat <fullgamefun@gmail.com>
2024-08-10 17:21:08 +0200

    Initial Commit

--- main/Package.swift
+++ main/Package.swift
@@ -11,10 +11,18 @@ let package = Package(
 		.library(name: "swiftcode", type: .static, targets: ["Main"]),
     ],
 
+	dependencies: [
+		.package(url: "https://github.com/anders0nmat/EspSwift.git", branch: "main"),
+	],
+
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
 
-		.target(name: "Main"),
+		.target(
+			name: "Main",
+			dependencies: [
+				.product(name: "EspSwift", package: "EspSwift"),
+			]),
     ]
 )
--- main/Source/Main/esp_main.swift
+++ main/Source/Main/esp_main.swift
@@ -1,6 +1,25 @@
 
+import WiFi
+import HttpServer
+import FreeRTOS
+
 @_cdecl("app_main")
 func app_main() {
 	print("🏎️  + 📦   Hello from an Embedded Swift Package")
+
+	WiFi.connect(
+		to: "<Your WiFi SSID>",
+		password: "<Your WiFi Password>",
+		securityLevel: .WPA2)
+
+	let server = try! HttpServer()
+	try! server.register(.GET, "/") { req in
+		req.respond(with: "Hello from esp32c6")
+	}
+
+	while true {
+		// Because HttpServer runs in its own thread, we only need to keep the `server`-object alive
+		Task.delay(for: .hours(1))
+	}
 }
 
