a7ae4170bc78726dd84e5d54a80783cc38f6bb1c
anders0nmat <fullgamefun@gmail.com>
2025-10-25 23:02:52 +0200

    Updated to match latest EspSwift module

--- main/Source/Main/esp_main.swift
+++ main/Source/Main/esp_main.swift
@@ -2,20 +2,29 @@
 import WiFi
 import HttpServer
 import FreeRTOS
+import EventLoop
+
 
 @_cdecl("app_main")
 func app_main() {
 	print("🏎️  + 📦   Hello from an Embedded Swift Package")
 
+    EventLoop.default.create()
+
 	WiFi.connect(
 		to: "<Your WiFi SSID>",
 		password: "<Your WiFi Password>",
 		securityLevel: .WPA2)
+    
+    print("WiFi connected.")
 
-	let server = try! HttpServer()
-	try! server.register(.GET, "/") { req in
-		req.respond(with: "Hello from esp32c6")
-	}
+    let server = try! HttpServer()
+
+    server.register(.GET, "/") { req in
+        req.respondWith("Hello, World!")
+    }
+
+    print("Http Server started.")
 
 	while true {
 		// Because HttpServer runs in its own thread, we only need to keep the `server`-object alive
